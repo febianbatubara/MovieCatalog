18f64f13f10acc3bb53b4a2eaca200bba54e3d0a
febianbatubara <febianbatubaraa@gmail.com>
2021-04-16 23:30:13 +0700

    add tab main layout and pager adapter

--- app/src/main/java/com/febian/android/moviecatalog/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/febian/android/moviecatalog/ui/splash/SplashActivity.kt
@@ -4,19 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.febian.android.moviecatalog.MainActivity
+import com.febian.android.moviecatalog.ui.home.HomeActivity
 import com.febian.android.moviecatalog.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_splash)
 
-        supportActionBar?.hide()
-
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
