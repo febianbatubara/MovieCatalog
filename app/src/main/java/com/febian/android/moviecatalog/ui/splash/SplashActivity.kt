3f664536ed9201939cb6e2b070b6598d705f9bd9
febianbatubara <febianbatubaraa@gmail.com>
2021-04-16 21:06:59 +0700

    add splash screen

---
+++ app/src/main/java/com/febian/android/moviecatalog/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.febian.android.moviecatalog.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.febian.android.moviecatalog.MainActivity
+import com.febian.android.moviecatalog.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
